88e00e42340f043dfa4cb5f7a8c5d44f7bd8d999
samara-belisaria <samarabelisaria18@gmail.com>
2026-03-08 22:13:05 -0300

    Minha atualização de hoje

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -5,9 +5,7 @@ var target: Node2D
 func _ready() -> void:
 	get_target()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	position = target.position
 
 func get_target():
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,39 +1,240 @@
 extends CharacterBody2D
 
+enum PlayerState {
+	idle,
+	walk,
+	jump,
+	fall,
+	duck,
+	slide,
+	dead
+}
+
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var hitbox_collision_shape: CollisionShape2D = $Hitbox/CollisionShape2D
 
+@onready var reload_timer: Timer = $ReloadTimer
 
-const SPEED = 80.0
+@export var max_speed = 180.0
+@export var acceleration = 400
+@export var deceleration = 400
+@export var slide_deceleration = 100
+
 const JUMP_VELOCITY = -300.0
 
+var jump_count = 0
+@export var max_jup_count = 2
+var direction = 0
+var status: PlayerState
+
+func _ready() -> void:
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	match status:
+		PlayerState.idle:
+			idle_state(delta)
+		PlayerState.walk:
+			walk_state(delta)
+		PlayerState.jump:
+			jump_state(delta)
+		PlayerState.fall:
+			fall_state(delta)
+		PlayerState.duck:
+			duck_state(delta)
+		PlayerState.slide:
+			slide_state(delta)
+		PlayerState.dead:
+			dead_state(delta)
+			
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func go_to_walk_state():
+	status = PlayerState.walk
+	anim.play("walk")
+
+func go_to_jump_state():
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+	jump_count += 1
+	
+func go_to_fall_state():
+	status = PlayerState.fall
+	anim.play("fall")
+	
+func go_to_duck_state():
+	status = PlayerState.duck
+	anim.play("duck")
+	set_small_collider()
+	
+func exit_from_duck_state():
+	set_large_collider()
+	
+func go_to_slide_state():
+	status = PlayerState.slide
+	anim.play("slide")
+	set_small_collider()
+	
+func exit_from_slide_state():
+	set_large_collider()
+	
+func go_to_dead_state():
+	if status == PlayerState.dead:
+		return
+	
+	status = PlayerState.dead
+	anim.play("dead")
+	velocity.x = 0
+	reload_timer.start()
+
+func idle_state(delta):
+	move(delta)
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+		
+	if Input.is_action_pressed("duck"):
+		go_to_duck_state()
+		return
+	
+func walk_state(delta):
+	move(delta)
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+		
+	if Input.is_action_just_pressed("duck"):
+		go_to_slide_state()
+		return
+		
+	if !is_on_floor():
+		jump_count += 1
+		go_to_fall_state()
+		return
+	
+func jump_state(delta):
+	move(delta)
+	
+	if Input.is_action_just_pressed("jump") && can_jump():
+		go_to_jump_state()
+		return
+	
+	if velocity.y > 0:
+		go_to_fall_state()
+		return
+		
+func fall_state(delta):
+	move(delta)
+	
+	if Input.is_action_just_pressed("jump") && can_jump():
+		go_to_jump_state()
+		return
 		
 	if is_on_floor():
-		if direction > 0:
-			anim.flip_h = false
-			anim.play("walk")
-		elif direction < 0:
-			anim.flip_h = true
-			anim.play("walk")
+		jump_count = 0
+		if velocity.x == 0:
+			go_to_idle_state()
 		else:
-			anim.play("idle")
+			go_to_walk_state()
+		return
+		
+func duck_state(_delta):
+	update_direction()
+	if Input.is_action_just_released("duck"):
+		exit_from_duck_state()
+		go_to_idle_state()
+		return
+		
+func slide_state(delta):
+	velocity.x = move_toward(velocity.x, 0, slide_deceleration * delta)
+	
+	if Input.is_action_just_released("duck"):
+		exit_from_slide_state()
+		go_to_walk_state()
+		return
+		
+	if velocity.x == 0:
+		exit_from_slide_state()
+		go_to_duck_state()
+		return
+		
+func dead_state(_delta):
+	pass
+		
+func move(delta):
+	update_direction()
+	
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * max_speed, acceleration * delta)
 	else:
-		anim.play("jump")
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
+		
+func update_direction():
+	direction = Input.get_axis("left", "right")
+	
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+	
+func can_jump() -> bool:
+	return jump_count < max_jup_count
+	
+func set_small_collider():
+	collision_shape.shape.radius = 5
+	collision_shape.shape.height = 10
+	collision_shape.position.y = 3
+	
+	hitbox_collision_shape.shape.size.y = 10
+	hitbox_collision_shape.position.y = 3
+	
+func set_large_collider():
+	collision_shape.shape.radius = 6
+	collision_shape.shape.height = 16
+	collision_shape.position.y = 0
+	
+	hitbox_collision_shape.shape.size.y = 15
+	hitbox_collision_shape.position.y = 0.5
 
-	move_and_slide()
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Enemies"):
+		hit_enemy(area)
+	elif area.is_in_group("LethalArea"):
+		hit_lethal_area()
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group("LethalArea"):
+		go_to_dead_state()
+
+func hit_enemy(area: Area2D):
+	if velocity.y > 0:
+		# inimigo morre
+		area.get_parent().take_damage()
+		go_to_jump_state()
+	else:
+		#player morre
+		go_to_dead_state()
+
+func hit_lethal_area():
+	go_to_dead_state()
+
+func _on_reload_timer_timeout() -> void:
+	get_tree().reload_current_scene()
